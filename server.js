48bbba5e667e91f7f7af50a6db88b9fa9ba50668
Juan Campos <juancampos.chk@gmail.com>
2024-09-09 11:00:53 -0300

    server

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.urlencoded({ extended: true }));
 // app.use(express.static('public'));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.set('views', './src/views');
+app.set('views', path.join(__dirname, 'src', 'views'));
 app.set('view engine', 'ejs');
 
 // sessões
